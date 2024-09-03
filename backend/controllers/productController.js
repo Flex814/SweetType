4bb85597464035e09e88a99daf75f534cd208651
jgomez <jgomez@babelstreet.com>
2024-09-03 09:41:55 -0400

    Working backend init

---
+++ backend/controllers/productController.js
@@ -0,0 +1,79 @@
+const Product = require('../models/Product');
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) return res.status(404).json({ msg: 'Product not found' });
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.createProduct = async (req, res) => {
+  const { name, description, price, inStock } = req.body;
+
+  try {
+    const newProduct = new Product({
+      name,
+      description,
+      price,
+      inStock,
+    });
+
+    const product = await newProduct.save();
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  const { name, description, price, inStock } = req.body;
+
+  const updatedProduct = { name, description, price, inStock };
+
+  try {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) return res.status(404).json({ msg: 'Product not found' });
+
+    product = await Product.findByIdAndUpdate(
+      req.params.id,
+      { $set: updatedProduct },
+      { new: true }
+    );
+
+    res.json(product);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    let product = await Product.findById(req.params.id);
+
+    if (!product) return res.status(404).json({ msg: 'Product not found' });
+
+    await Product.findByIdAndRemove(req.params.id);
+
+    res.json({ msg: 'Product removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json());
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/products', require('./routes/products'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
